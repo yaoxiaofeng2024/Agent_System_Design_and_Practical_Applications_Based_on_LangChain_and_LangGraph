78105dbba5f192a7e38e7917b1908f2bac0c28bb
Your Name <you@example.com>
2026-05-29 15:28:15 +0800

    update

--- init_client.py
+++ init_client.py
@@ -4,9 +4,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-deepseek_api = os.environ['DEEPSEEK_API']
-deepseek_url = os.environ['DEEPSEEK_URL']
-deepseek_model = os.environ['DEEPSEEK_MODEL']
+#deepseek_api = os.environ['DEEPSEEK_API']
+#deepseek_url = os.environ['DEEPSEEK_URL']
+#deepseek_model = os.environ['DEEPSEEK_MODEL']
+deepseek_api = 'sk-b444e6b29b344981bbd848db8fe7fe0b'
+deepseek_url = 'https://api.deepseek.com'
+deepseek_model = 'deepseek-v4-pro'
 
 def init_llm(temperature):
     deepseek_client = ChatOpenAI(api_key=deepseek_api,
